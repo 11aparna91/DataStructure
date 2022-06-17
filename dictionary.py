7c6908c45a67b520d022ae1418d463e354060127
Aparna Popade <popadep@gmail.com>
2022-06-17 08:18:39 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -6,3 +6,15 @@ dict1= [1,2,1,3,4,1,1,1]
 print(Counter(dict1))
 
 ############# Code 2 #############
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict2={}
+        
+        for i in nums:
+            if i not in dict2:
+                dict2[i]=1
+            else:
+                dict2[i]+=1
+                return True
+        
+        return False
