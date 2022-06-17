d8fc8e4e93567ac6a8d0e893c9aab3dde546ff4d
Aparna Popade <popadep@gmail.com>
2022-06-16 23:39:17 -0700

    Update list.py

--- list.py
+++ list.py
@@ -1,1 +1,7 @@
-
+######### List.append() method ###########
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        lis=[]
+        for i in range(1,n+1):
+          lis.append(str(i))
+        return lis
