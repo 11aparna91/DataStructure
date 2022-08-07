e25af48e0c443705abbe608f005350399a6479e4
Aparna Popade <popadep@gmail.com>
2022-08-07 12:29:26 -0700

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -23,3 +23,20 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
         return slow
+###################################
+########Reverse the linked list
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        rev=None
+        while head:
+            cur=head
+            head=head.next
+            cur.next=rev
+            rev=cur
+        return rev
