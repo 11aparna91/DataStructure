f8397661d45f8e93a3355fad1dabcf94ca0e04e2
Aparna Popade <popadep@gmail.com>
2022-08-16 15:57:06 -0700

    Update list.py

--- list.py
+++ list.py
@@ -5,3 +5,10 @@ class Solution:
         for i in range(1,n+1):
           lis.append(str(i))
         return lis
+################ reverse a list ###########
+
+class Solution:
+    def reverse(self, nums,l,r):
+        while l<r:
+                nums[l], nums[r] =  nums[r], nums[l]
+                l,r= l+1, r-1
