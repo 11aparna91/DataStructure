86ece54ffc925966a4360131be7709fbf8070189
Aparna Popade <popadep@gmail.com>
2022-08-20 20:09:40 -0700

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -70,5 +70,27 @@ class Solution:
             tail.next=list2
         
         return dummy.next
+     
+     
+#########################################
+
+######### Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def detectCycle(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast: break
+        else: return None  # if not (fast and fast.next): return None
+        while head != slow:
+            head, slow = head.next, slow.next
+        return head
             
         
