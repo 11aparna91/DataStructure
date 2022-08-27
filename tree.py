109b9829267148eca50788abc5c35ebcd7149efd
Aparna Popade <popadep@gmail.com>
2022-08-27 12:32:23 -0700

    Update tree.py

--- tree.py
+++ tree.py
@@ -58,4 +58,27 @@ class Solution:
         root.left= self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None)
         root.right= self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
         return root
+    
+####################################################
+###### Problem 200 ################################    
+    
+    class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        NumOfIsland=0
+        def dfs(i,j):
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+                return
+            grid[i][j]="0"
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+            
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    dfs(i,j)
+                    NumOfIsland +=1
+                
+        return NumOfIsland
            
