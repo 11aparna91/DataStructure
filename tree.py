c9f3eedb76ff83ee6a0d4975c6e756b0cf7aa83f
Aparna Popade <popadep@gmail.com>
2022-08-28 20:27:28 -0700

    Update tree.py

--- tree.py
+++ tree.py
@@ -59,7 +59,7 @@ class Solution:
         root.right= self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
         return root
     
-####################################################
+#############   dfs       #############
 ###### Problem 200 ################################    
     
     class Solution:
@@ -81,4 +81,22 @@ class Solution:
                     NumOfIsland +=1
                 
         return NumOfIsland
+    
+#### Problem Number 733 ###########
+###### Time Complexity= O(n) #####
+####### Space Complexity= O(n) ###
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        prev_color= image[sr][sc]
+        if prev_color==color:
+            return image
+        def dfs(r,c):
+            if image[r][c]==prev_color:
+                image[r][c]=color
+                if r>=1: dfs(r-1,c)
+                if c>=1: dfs(r,c-1)
+                if r<len(image)-1: dfs(r+1,c)
+                if c<(len(image[0])-1): dfs(r,c+1)
+        dfs(sr,sc)
+        return image
            
