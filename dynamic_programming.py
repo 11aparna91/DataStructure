bb008f19281947ae36fcd1ffd5844b0d063f93f0
Aparna Popade <popadep@gmail.com>
2022-08-29 19:11:35 -0700

    Update dynamic_programming.py

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -21,3 +21,14 @@ class Solution:
             dp.insert(i, dp[i - 1] + dp[i - 2])
             
         return dp[n] 
+############### Space Complexity=O(m*n) #############
+##############  Problem Number 62 ###################
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if not m or not n:
+            return 0
+        dp = [[1 for i in range(n)] for j in range(m)]
+        for i in range(1,m):   ## consider reverse matrix
+            for j in range(1,n):
+                dp[i][j]= dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
