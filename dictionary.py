287c2fd948036f65564938178bb9d251ed2ded40
Aparna Popade <popadep@gmail.com>
2022-09-12 11:16:54 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -79,4 +79,17 @@ class Solution:
                 count +=1
             set1.add(val)
         return count    
+######################### Problem Number 1347 ############
+
+from collections import Counter
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        cnt1=Counter(s)
+        cnt2=Counter(t)
+        sum=0
+        for key,value in cnt1.items():
+            if value>cnt2[key]:
+                sum += (value - cnt2[key])
+        return sum
+
 
